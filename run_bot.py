c6ed178a8667a6b0ff0a151b3b0ef684f1f7adf6
Muhammadjon Yangiboyev <kinodanparcha@gmail.com>
2025-09-26 20:56:33 +0500

    Removed start_bot.py, cleaned run_bot.py to use env variables

--- run_bot.py
+++ run_bot.py
@@ -3,12 +3,13 @@
 Script to run the bot with the token set as environment variable.
 """
 import os
-import sys
+from bot import main
 
-# Set the bot token and target channel as environment variables
-os.environ['BOT_TOKEN'] = '8338311677:AAG7f_T9GFhIzJwSSxagPGlOIC7sEO6q0Io'
-os.environ['TARGET_CHANNEL'] = '-1003038072399'  # Your private channel ID
-os.environ['ADMIN_USER_ID'] = '1997334476'  # Set to your actual user ID
+if __name__ == '__main__':
+    os.environ['BOT_TOKEN'] = os.getenv("BOT_TOKEN")
+    os.environ['TARGET_CHANNEL'] = os.getenv("TARGET_CHANNEL")
+    os.environ['ADMIN_USER_ID'] = os.getenv("ADMIN_USER_ID")
+    main()
 
 # Now import and run the bot
 from bot import main
--- stop_bot.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo 🛑 Stopping Telegram Bot...
-echo.
-python stop_bot.py
-pause
-
-
